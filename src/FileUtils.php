9aae5b0b41e040200dcc6d2368dded2b22a4a0b2
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2025-03-12 16:50:32 +0300

    Updated readHashMap.

--- src/FileUtils.php
+++ src/FileUtils.php
@@ -9,7 +9,7 @@ class FileUtils
         $result = [];
         $fh = fopen($fileName, 'r');
         while ($line = fgets($fh)) {
-            $list = preg_split($line, " ");
+            $list = explode(" ", $line);
             if (count($list) == 2) {
                 $result[$list[0]] = $list[1];
             }
